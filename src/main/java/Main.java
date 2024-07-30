afdbd307af4c2c1390ddfa39b63d57666ef32bb0
Harsh <harshchetwani2002@gmail.com>
2024-07-30 19:46:42 +0530

    pass stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,41 +5,37 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-
 public class Main {
-    public static void main(String[] args) {
-        // You can use print statements as follows for debugging, they'll be visible
-        // when running tests.
-        System.out.println("Logs from your program will appear here!");
-
-        ServerSocket serverSocket = null;
-        Socket clientSocket = null;
-        try {
-            serverSocket = new ServerSocket(4221);
-            serverSocket.setReuseAddress(true);
-            clientSocket = serverSocket.accept(); // Wait for connection from client.
-            InputStream input = clientSocket.getInputStream();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-            String line = reader.readLine();
-            System.out.println(line);
-            String[] HttpRequest = line.split(" ", 0);
-            OutputStream output = clientSocket.getOutputStream();
-
-            if (HttpRequest[1].equals("/")) {
-                output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-            } else {
-                output.write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
-            }
-            System.out.println("Accepted new connection");
-
-            // Close the streams and socket
-            reader.close();
-            output.close();
-            clientSocket.close();
-            serverSocket.close();
-
-        } catch (IOException e) {
-            System.out.println("IOException: " + e.getMessage());
-        }
+  public static void main(String[] args) {
+    // You can use print statements as follows for debugging, they'll be visible
+    // when running tests.
+    System.out.println("Logs from your program will appear here!");
+    ServerSocket serverSocket = null;
+    Socket clientSocket = null;
+    try {
+      serverSocket = new ServerSocket(4221);
+      serverSocket.setReuseAddress(true);
+      clientSocket = serverSocket.accept(); // Wait for connection from client.
+      System.out.println("accepted new connection");
+      InputStream input = clientSocket.getInputStream();
+      BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+      String line = reader.readLine();
+      String[] HttpRequest = line.split(" ", 0);
+      if (HttpRequest[1].equals("/")) {
+        clientSocket.getOutputStream().write(
+            "HTTP/1.1 200 OK\r\n\r\n".getBytes());
+      } else if (HttpRequest[1].startsWith("/echo/")) {
+        String msg = HttpRequest[1].substring(6);
+        String header = String.format(
+            "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s",
+            msg.length(), msg);
+        clientSocket.getOutputStream().write(header.getBytes());
+      } else {
+        clientSocket.getOutputStream().write(
+            "HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
+      }
+    } catch (IOException e) {
+      System.out.println("IOException: " + e.getMessage());
     }
+  }
 }
